3f788a3180032e18d4aec3fd4331460b88724284
codeboss-fav <favourolaide47@gmail.com>
2020-02-10 00:42:18 +0100

    added the razzle dazzle all around, with some fun animations

---
+++ assets/main.js
@@ -0,0 +1,19 @@
+gsap.from(
+    ".main-text", {
+        duration:2, 
+        y:500, 
+        opacity: 0,
+        delay: 3,
+        ease:"slow"});
+gsap.from(
+    ".sup-text", {
+        duration:3, 
+        y:400, 
+        delay: 4,
+        ease:"slow"});
+gsap.from(
+    ".why-link", {
+        duration:3, 
+        x: -400, 
+        // delay: 4,
+        ease:"slow"});
