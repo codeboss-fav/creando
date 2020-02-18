91e7d5925152b7eb69869a3e1fc8115b1a0a9da8
codeboss-fav <favourolaide47@gmail.com>
2020-02-18 04:55:51 +0100

    added a smooth float effect to the illustrations , removed the scrool snap behaviour for mobile devices

--- assets/main.js
+++ assets/main.js
@@ -17,4 +17,14 @@ gsap.from(
         x: -400, 
         // delay: 4,
         ease:"slow"});
-        AOS.init();
+// anumate on scroll
+AOS.init();
+// animate on scroll
+
+var tl = new TimelineMax();
+
+tl.from(".des-img", 2.0, { y: -42, });
+tl.to(".des-img", 1.5, { y: 15, ease:"slow", repeat:-1, yoyo:true });
+
+tl.from(".dev-img", 2.0, { y: -40, });
+tl.to(".dev-img", 1.5, { y: 20, ease:"slow", repeat:-1, yoyo:true });
